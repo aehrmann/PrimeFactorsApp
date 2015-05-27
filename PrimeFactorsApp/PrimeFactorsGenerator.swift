3ae230ebae775a39e81b7b60b0980dd0eea52db8
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-05-27 12:41:25 -0500

    Add tested PrimeFactorsGenerator

---
+++ PrimeFactorsApp/PrimeFactorsGenerator.swift
@@ -0,0 +1,24 @@
+//
+//  PrimeFactorsGenerator.swift
+//  PrimeFactorsApp
+//
+//  Created by Ariel Ehrmann on 5/27/15.
+//  Copyright (c) 2015 AriAndLisa. All rights reserved.
+//
+
+import Foundation
+
+class PrimeFactorsGenerator {
+    func generate(var number: Int) -> [Int] {
+        var factors = [Int]()
+        var currentFactor = 2
+        while number > 1 {
+            while number % currentFactor == 0 {
+                factors.append(currentFactor)
+                number = number/currentFactor
+            }
+            currentFactor++
+        }
+        return factors
+    }
+}
---
+++ PrimeFactorsAppSpecs/PrimeFactorsGeneratorSpec.swift
@@ -0,0 +1,61 @@
+//
+//  PrimeFactorsGeneratorSpec.swift
+//  PrimeFactorsApp
+//
+//  Created by Ariel Ehrmann on 5/27/15.
+//  Copyright (c) 2015 AriAndLisa. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class PrimeFactorsGeneratorSpec: QuickSpec {
+    let generator = PrimeFactorsGenerator()
+
+    func generatePrimes(number: Int) -> [Int] {
+        return generator.generate(number)
+    }
+
+    override func spec() {
+        describe("Prime Factors Generator") {
+            it("generates no prime factors for the number 1") {
+                expect(self.generatePrimes(1)).to(equal([]))
+            }
+
+            it("generates prime factors for 2") {
+                expect(self.generatePrimes(2)).to(equal([2]))
+            }
+
+            it("generates prime factors for 3") {
+                expect(self.generatePrimes(3)).to(equal([3]))
+            }
+
+            it("generates prime factors for 4") {
+                expect(self.generatePrimes(4)).to(equal([2, 2]))
+            }
+
+            it("generates prime factors for 6") {
+                expect(self.generatePrimes(6)).to(equal([2, 3]))
+            }
+
+            it("generates prime factors for 8") {
+                expect(self.generatePrimes(8)).to(equal([2, 2, 2]))
+            }
+
+            it("generates prime factors for 9") {
+                expect(self.generatePrimes(9)).to(equal([3, 3]))
+            }
+
+            it("generates prime factors for 25") {
+                expect(self.generatePrimes(25)).to(equal([5, 5]))
+            }
+
+            it("generates prime factors for 2 * 2 * 3 * 5 * 7") {
+                expect(self.generatePrimes(2 * 2 * 3 * 5 * 7)).to(equal([2, 2, 3, 5, 7]))
+            }
+
+
+        }
+    }
+}
