288dce0a711edadc94937962441eb96f4659932f
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-05-27 10:37:44 -0500

    Add HomeViewController

---
+++ PrimeFactorsAppSpecs/HomeViewControllerSpec.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewControllerSpec.swift
+//  PrimeFactorsApp
+//
+//  Created by Ariel Ehrmann on 5/27/15.
+//  Copyright (c) 2015 AriAndLisa. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class HomeViewControllerSpec: QuickSpec {
+    override func spec() {
+
+    }
+}
