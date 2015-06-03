e3daaef89a5ca85069ee22b68672734722d9a292
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-06-03 17:01:54 -0500

    Finish ErrorLabelManager
    * sets visibility correctly
    * sets error message correctly

--- PrimeFactorsApp/ErrorLabelManager.swift
+++ PrimeFactorsApp/ErrorLabelManager.swift
@@ -2,12 +2,33 @@ import UIKit
 
 public class ErrorLabelManager {
     let label: UILabel
+    private let characterLimit = 12
 
     public init(label: UILabel) {
         self.label = label
     }
 
     public func update(userInput: String) {
-        label.hidden = true
+        if userInput.toInt() != nil || userInput.isEmpty {
+            label.hidden = true
+        } else {
+            label.hidden = false
+            label.text = createErrorMessage(userInput)
+        }
     }
+
+    private func createErrorMessage(inputString: String) -> String {
+        let displayString = formatForDisplay(inputString: inputString)
+        return "\"\(displayString)\" is not an integer"
+    }
+
+    private func formatForDisplay(#inputString: String) -> String {
+        var displayString = inputString
+        if count(inputString) >= self.characterLimit {
+            let substringRange = Range(start: inputString.startIndex, end: advance(inputString.startIndex, self.characterLimit))
+            displayString = inputString.substringWithRange(substringRange) + "..."
+        }
+        return displayString
+    }
+
 }
--- PrimeFactorsAppSpecs/ErrorLabelManagerSpec.swift
+++ PrimeFactorsAppSpecs/ErrorLabelManagerSpec.swift
@@ -23,23 +23,61 @@ class ErrorLabelManagerSpec: QuickSpec {
                     manager = ErrorLabelManager(label: label)
                 }
 
-                context("when input is valid") {
-                    it("hides the label") {
-                        let userInput = "2"
-                        manager.update(userInput)
-                        expect(label.hidden).to(beTrue())
+                describe("displaying the label") {
+                    context("when input is valid") {
+                        it("hides the label") {
+                            let userInput = "2"
+                            manager.update(userInput)
+                            expect(label.hidden).to(beTrue())
+                        }
+                    }
+
+                    context("when input is invalid") {
+                        it("shows the label") {
+                            let userInput = "not valid"
+                            manager.update(userInput)
+                            expect(label.hidden).to(beFalse())
+                        }
+                    }
+
+                    context("when input is empty") {
+                        it("hides the label") {
+                            let userInput = ""
+                            manager.update(userInput)
+                            expect(label.hidden).to(beTrue())
+                        }
                     }
                 }
 
-                context("when input is invalid") {
-                    it("shows the label") {
-                        let userInput = "not valid"
-                        manager.update(userInput)
+                describe("setting the label's text") {
+                    
+                    context("when input length is less than or equal to the allowed number of characters") {
 
-                        expect(label.hidden).to(beFalse())
+                        it("displays an error message with the invalid input") {
+                            let userInput = "not valid"
+                            let errorMessage = "\"\(userInput)\" is not an integer"
+
+                            manager.update(userInput)
+
+                            expect(label.text).to(equal(errorMessage))
+                        }
+
                     }
-                }
 
+                    context("when input length is greater than the allowed number of characters") {
+                        it("displays an error message with the truncated invalid input") {
+                            let userInput = "not an integer"
+                            let truncatedInput = "not an integ..."
+                            let errorMessage = "\"\(truncatedInput)\" is not an integer"
+
+                            manager.update(userInput)
+
+                            expect(label.text).to(equal(errorMessage))
+                        }
+                        
+                    }
+
+                }
             }
         }
     }
